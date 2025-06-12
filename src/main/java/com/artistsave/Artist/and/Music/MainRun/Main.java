0ae58df9dc385f84dbf19245ab8b6562a2308ce6
klinsmann lucas <multiklinsmann@gmail.com>
2025-06-12 10:29:07 -0300

    Add config postgresql and creat new main on commandlinerunner.

---
+++ src/main/java/com/artistsave/Artist/and/Music/MainRun/Main.java
@@ -0,0 +1,16 @@
+package com.artistsave.Artist.and.Music.MainRun;
+
+public class Main {
+    public static void menu (){
+        var menu = """
+                1- Cadastrar artistas
+                2- Cadastrar músicas
+                3- Listar músicas
+                4- Buscar músicas por artistas
+                5- Pesquisar dados sobre um artista
+                
+                9- Sair
+                """;
+        System.out.println(menu);
+    }
+}
---
+++ src/test/java/com/artistsave/Artist/and/Music/ArtistAndMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artistsave.Artist.and.Music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistAndMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
